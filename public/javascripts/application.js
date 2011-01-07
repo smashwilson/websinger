94d39189451d43642482552a37989ba5f630fd56
Ash Wilson <smashwilson@gmail.com>
2011-01-06 20:49:12 -0500

    Implemented default AJAX callbacks.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,21 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(function() {
+
+  // By default, AJAX function responses should render in the #success or #failure divs of the application layout.
+  // Handle any that are allowed to propagate to the document object.
+  $('body').bind('ajax:success', function (event, data, status, xhr) {
+    $('#success')
+      .show()
+      .html(data)
+      .click(function () { $(this).fadeOut('slow') })
+  })
+  
+  $('body').bind('ajax:error', function (event, xhr, status, error) {
+    $('#failure')
+      .show()
+      .html(xhr.responseText)
+      .click(function () { $(this).fadeOut('slow') })
+  })
+})
