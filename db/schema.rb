fece8abd4eafd6e98e206019bf0330c62d1b28b2
Ash Wilson <smashwilson@gmail.com>
2011-01-23 14:56:20 -0500

    Define database indices.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101231210526) do
+ActiveRecord::Schema.define(:version => 20110123195119) do
 
   create_table "enqueued_tracks", :force => true do |t|
     t.integer  "position"
@@ -30,5 +30,9 @@ ActiveRecord::Schema.define(:version => 20101231210526) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "tracks", ["album"], :name => "index_tracks_on_album"
+  add_index "tracks", ["artist"], :name => "index_tracks_on_artist"
+  add_index "tracks", ["title"], :name => "index_tracks_on_title"
 
 end
