2fef2d02a76b2b9422ea1e880dd128707824f7ee
Ash Wilson <smashwilson@gmail.com>
2011-01-22 22:07:47 -0500

    Heartbeat function.

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,2 +1,19 @@
 module PlayersHelper
+
+  def command_link command, enabled = true, args = {}
+    args[:method] = 'PUT'
+    args[:remote] = true
+    args[:id] = "command_#{command}"
+    args[:class] = 'disable' unless enabled
+    link_to command, player_path(:command => command), args
+  end
+  
+  def percent_complete
+    @current_track ? @current_track.percent_complete(@status.seconds) : 0
+  end
+  
+  def time_s
+    @current_track ? "#{@status.seconds_s} / #{@current_track.length_s}" : ''
+  end
+
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,37 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
+var heartbeatCallbacks = []
+var heartbeatDelay = 2000
+
+// A heartbeat function that will be called every few seconds.
+function heartbeat(callback) {
+  heartbeatCallbacks.push(callback)
+}
+
+// Invoke the updates manually.
+function beat() {
+  $(heartbeatCallbacks).each(function () { this() })
+}
+
+// A utility function to format a duration in seconds as "MM:SS"
+function toMinutes(seconds) {
+  minutes = Math.floor(seconds / 60)
+  left = Math.round(seconds - (minutes * 60))
+  if( left < 10 ) { left = '0' + left }
+  return minutes + ':' + left
+}
+
 $(function() {
 
   // By default, AJAX function responses should render in the #success or #failure divs of the application layout.
   // Handle any that are allowed to propagate to the document object.
   $('body').bind('ajax:success', function (event, data, status, xhr) {
-    $('#success')
-      .show()
-      .html(data)
+    if( ! data.match(/^\W+$/) ) {
+      $('#success')
+        .show()
+        .html(data)
+    }
   })
   
   $('body').bind('ajax:error', function (event, xhr, status, error) {
@@ -21,4 +44,40 @@ $(function() {
   fadeCallback = function () { $(this).fadeOut('slow') }
   $('#success').click(fadeCallback)
   $('#failure').click(fadeCallback)
+  
+  // Update the player state.
+  function refreshPlayer() {
+    $.getJSON('player', function (status) {
+      var percent = 0
+      var timeString = ''
+      
+      if( status.playback_state != 'playing' ) {
+        $('#command_play').removeClass('disable')
+        $('#command_pause').addClass('disable')
+      } else {
+        $('#command_play').addClass('disable')
+        $('#command_pause').removeClass('disable')
+      }
+    
+      $('.player .track .artist').html(status.artist)
+      $('.player .track .title').html(status.title)
+      
+      if( 'track_length' in status ) {
+        percent = (status.seconds / status.track_length) * 100
+        timeString = toMinutes(status.seconds) + ' / ' + toMinutes(status.track_length)
+      }
+      $('.player .progress .bar').css('width', percent + '%')
+      $('.player .progress span').html(timeString)
+    })
+  }
+  $('.player .controls a').bind('click', function () { refreshPlayer() })
+  
+  heartbeat(refreshPlayer);
+  
+  // Execute the heartbeat callback.
+  function periodic() {
+    beat()
+    setTimeout(periodic, heartbeatDelay)
+  }
+  periodic()
 })
