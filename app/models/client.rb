fb644a8edb1358d00d9a1bc8fbf82c7b46458964
Ash Wilson <smashwilson@gmail.com>
2012-03-04 14:10:55 -0500

    Reconsolidate the Client model. #16

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,5 @@
 require 'mpg123player/common'
+require 'active_support/json'
 
 # Non-ActiveRecord model. Manage status and communications with the mpg123 player process.
 class Client
@@ -11,21 +12,53 @@ class Client
     configure
   end
 
-  # Issue a command to the server, synchronously if appropriate.
+  # Issue a command to the server and wait for the remote process to handle it, within the currently configured
+  # +command_timeout+.
   def command string, parameter = nil
-    raise '#command not implemented'
+    cmd = PlayerCommand.create!(:action => string, :parameter => parameter)
+
+    Rails.logger.silence(WARN) do
+      wait_time = 0
+      until wait_time >= @command_timeout
+        wait_time += sleep(@command_poll)
+        return true unless PlayerCommand.exist?(cmd.id)
+      end
+    end
+
+    # Timed out waiting for command acceptance
+    false
   end
 
   # Status
 
-  # Return false and set +error+ if the player is in a bad state.
+  # Query the current player status, setting +error+ and returning false if something is wrong.
   def ok?
+    unless File.readable?(@pid_path)
+      @error = "Can't read the pid file at #{@pid_path}!"
+      return false
+    end
+    pid = File.open(@pid_path) { |f| f.gets(nil) }.to_i
+    Process.kill(0, pid)
+  rescue Errno::ESRCH => e
+    @error = "The server died!"
+    false
+  rescue Errno::EPERM => e
+    @error = "Someone else is running the server!"
+    false
+  else
     true
   end
 
-  # Return a Status object representing the current state of the player, ready for JSON rendering.
+  # Populate the Status object by reading the JSON written to disk by the server process, amending it with our local
+  # +error+ if one is present.
   def status
-    Status.stopped
+    @status ||= if File.readable?(@status_path)
+      Status.from(JSON(File.read(@status_path)))
+    else
+      Status.stopped
+    end
+    @status.error = @error
+    @status
   end
 
   def playing?
--- app/models/test_client.rb
+++
@@ -1,4 +0,0 @@
-# A Client implementation for use in test cases. Requires explicit methods to drive its lifecycle (advancing through
-# tracks and so on).
-class TestClient < Client
-end
