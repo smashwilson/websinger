757f13d24397df74fb2d47c732953bf5f62ee935
Ash Wilson <smashwilson@gmail.com>
2013-12-08 13:33:37 -0500

    This is way too old and untested.
    Let's start from scratch, and add stuff back in one model at a time.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Websinger::Application.config.secret_token = '3246232a570216c142f10a5610ff598097c45c45b845400e3b41bd51178a8b31d2717e0f21b6b1d280b258d1d305b65baa6a72c3f41ebdab6980606f5d33714b'
