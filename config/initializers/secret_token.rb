c1e169ebb3f30b71a88d0e980415b84301c5c2ee
Ash Wilson <smashwilson@gmail.com>
2013-12-08 13:49:35 -0500

    Rails 4 template, minus Test::Unit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Websinger::Application.config.secret_key_base = 'dcd7d62688ebe041a55af521478ac02b86c22898a42ad7ca73300c5b33c16992d0ed3c67dbacf9fc49b689ebf60043577f78ecfd7a5951f37e9c20c8ee7869a9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Websinger::Application.config.session_store :cookie_store, key: '_websinger_session'
