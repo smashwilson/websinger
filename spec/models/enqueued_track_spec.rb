c6b5d42690b2b3273e1db96660fbba9dbbd2213b
Ash Wilson <smashwilson@gmail.com>
2013-12-12 20:53:39 -0500

    Specs for EnqueuedTrack.
    Bonus: they all pass! :sparkles:

--- spec/models/enqueued_track_spec.rb
+++ spec/models/enqueued_track_spec.rb
@@ -1,28 +1,70 @@
 require 'spec_helper'
 
 describe EnqueuedTrack do
-  it "must be associated with a track"
+  let(:e1) { create :enqueued_track, position: 1 }
+  let(:e2) { create :enqueued_track, position: 2 }
+  let(:e3) { create :enqueued_track, position: 3 }
+  let(:playlist) { [e1, e2, e3] }
 
-  it "returns all enqueued tracks in position order with #playlist"
+  it "must be associated with a track" do
+    build(:enqueued_track, track: nil).should_not be_valid
+  end
+
+  it "returns all enqueued tracks in position order with #playlist" do
+    playlist
+    EnqueuedTrack.playlist.should == playlist
+  end
 
   describe "#enqueue" do
-    it "enqueues a track at the end of the playlist by default"
-    it "enqueues a track at the start of the playlist with :top"
+    let(:track) { create :track }
+
+    it "enqueues a track at the end of the playlist by default" do
+      e = EnqueuedTrack.enqueue track
+      EnqueuedTrack.playlist.last.should == e
+    end
+
+    it "enqueues a track at the start of the playlist with :top" do
+      e = EnqueuedTrack.enqueue track, :top
+      EnqueuedTrack.playlist.first.should == e
+    end
   end
 
   describe "#enqueue_all" do
-    it "enqueues multiple tracks at the end of the playlist"
-    it "enqueues multiple tracks at the start of the playlist"
+    let(:t1) { create :track }
+    let(:t2) { create :track }
+    let(:both) { [t1, t2] }
+
+    before { playlist }
+
+    it "enqueues multiple tracks at the end of the playlist" do
+      es = EnqueuedTrack.enqueue_all both
+      EnqueuedTrack.playlist.should == playlist + es
+    end
+
+    it "enqueues multiple tracks at the start of the playlist" do
+      es = EnqueuedTrack.enqueue_all both, :top
+      EnqueuedTrack.playlist.should == es + playlist
+    end
   end
 
   describe "#top" do
     context "with no tracks enqueued" do
-      it "returns nil if no tracks are enqueued"
+      it "returns nil if no tracks are enqueued" do
+        EnqueuedTrack.top.should be_nil
+      end
     end
 
     context "with tracks enqueued" do
-      it "returns the track with the lowest position"
-      it "deletes the track"
+      before { playlist }
+
+      it "returns the track with the lowest position" do
+        EnqueuedTrack.top.should == e1
+      end
+
+      it "deletes the track from the playlist" do
+        EnqueuedTrack.top
+        EnqueuedTrack.playlist.should_not include(e1)
+      end
     end
   end
 end
